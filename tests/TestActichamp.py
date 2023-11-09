67bc661713bb1f6d1d9b9dc48c004f1fdf0505c1
Andrey Morozov <and.morozov.v@gmail.com>
2023-11-09 11:53:01 +0300

    add test for actichamp

--- tests/TestActichamp.py
+++ tests/TestActichamp.py
@@ -1,4 +1,6 @@
+import numpy as np
 import unittest
+import time
 from actichamp_w import ActiChamp
 
 
@@ -7,8 +9,27 @@ class TestActiChamp(unittest.TestCase):
     def setUp(self):
         self.amp = ActiChamp()
 
-    def test_loadLib(self):
+    def test_01_loadLib(self):
+        """
+        Сhecking DLL library loading.
+        :return:
+        """
         self.assertIsNotNone(self.amp.lib)
+
+    def test_02_get_data(self):
+        d = None
+        self.amp.open()
+        if self.amp.devicehandle != 0:
+            self.amp.start()
+            time.sleep(0.1)
+            d, disconnected = self.amp.read(
+                indices=np.array([0, 1]),
+                eegcount=2,
+                auxcount=0
+            )
+            self.amp.stop()
+        self.amp.close()
+        self.assertIsNotNone(d)
 
 
 if __name__ == "__main__":
