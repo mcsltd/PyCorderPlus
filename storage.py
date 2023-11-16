f470df4846227d4d781089aaab9ffea7823e35fe
Andrey Morozov <and.morozov.v@gmail.com>
2023-11-16 19:17:51 +0300

    added method process_start and initialized auxiliary methods

--- storage.py
+++ storage.py
@@ -30,6 +30,7 @@ along with PyCorder. If not, see <http://www.gnu.org/licenses/>.
 
 B{Revision:} $LastChangedRevision: 206 $
 """
+import time
 
 from modbase import *
 import platform
@@ -45,6 +46,7 @@ class StorageVision(ModuleBase):
     Vision Date Exchange Format
     - Storage class using ctypes
     """
+
     def __init__(self, *args, **kwargs):
         super().__init__(queuesize=50, name="StorageVision", **kwargs)
 
@@ -107,16 +109,235 @@ class StorageVision(ModuleBase):
         """
         Set all module parameters to default values
         """
-        self.default_path = ""          #: default data storage path
-        self.default_prefix = ""        #: prefex for data files e.g. "EEG_"
-        self.default_numbersize = 6     #: number of digits to append to file name
-        self.default_autoname = False   #: create auto file name
+        self.default_path = ""  #: default data storage path
+        self.default_prefix = ""  #: prefex for data files e.g. "EEG_"
+        self.default_numbersize = 6  #: number of digits to append to file name
+        self.default_autoname = False  #: create auto file name
 
+    def process_start(self):
+        """
+        Start data acquisition
+        """
+        # reset sample counter check
+        self.missing_timer = time.clock()
+        self.missing_interval = 0
+        self.missing_cumulated = 0
+        self.next_samplecounter = -2
+        # ToDo: enable recording button
+        # if self.params.recording_mode != RecordingMode.IMPEDANCE:
+        #     self.online_cfg.pushButtonRecord.setEnabled(True)
+        # else:
+        #     self.online_cfg.pushButtonRecord.setEnabled(False)
+
+    def process_input(self, datablock):
+        """
+        Store data to file
+        """
+        self.dataavailable = True
+        self.data = datablock
+
+        # keep last impedance values for next EEG header file
+        if self.data.recording_mode == RecordingMode.IMPEDANCE:
+            self.last_impedance = copy.copy(datablock)
+            return
+
+        # check sample counter
+        if self.next_samplecounter < -1:
+            self.next_samplecounter = self.data.sample_channel[0][0] - 1  # first block after start
+        samples = len(self.data.sample_channel[0])
+        missing_precheck = self.data.sample_channel[0][-1] - (self.next_samplecounter + samples)
+        self.marker_newseg = True  # always write new segment markers if samples are missing
+
+        # counter not in expected range ?
+        if missing_precheck != 0:
+            sct = self.data.sample_channel[0]
+            sct_check = np.append(self.next_samplecounter, sct)
+            sctDiff = np.diff(sct_check) - 1
+            sctBreak = np.nonzero(sctDiff)[0]
+            missing_samples = np.sum(sctDiff)
+            self.missing_interval += missing_samples
+            self.missing_cumulated += missing_samples
+            sctBreakDiff = np.array([sct_check[sctBreak + 1], sctDiff[sctBreak]])  # samplecounter / missing
+            if time.process_time() - self.missing_timer > 30:
+                self.missing_interval = missing_samples
+            # print("samples missing = %i, interval = %i, cumulated = %i"%(missing_samples, self.missing_interval, self.missing_cumulated))
+            error = "%d samples missing" % missing_samples
+            if self.missing_interval > 2:
+                self.send_event(
+                    ModuleEvent(self._object_name, EventType.ERROR, info=error, severity=ErrorSeverity.NOTIFY))
+                self.missing_interval = 0
+                self.missing_cumulated = 0
+            else:
+                self.send_event(ModuleEvent(self._object_name, EventType.LOG, info=error))
+            self.missing_timer = time.process_time()
+        else:
+            missing_samples = 0
+            sctBreakDiff = np.array([[], []], dtype=np.int64)
+
+        # set counter to the expected start sample number of next data block
+        self.next_samplecounter = self.data.sample_channel[0, -1]
+
+        if (self.data_file != 0) and not self.write_error:
+            try:
+                t = time.process_time()
+                # convert data to float and write to data file
+                d = datablock.eeg_channels.transpose()
+                f = d.flatten().astype(np.float32)
+                sizeof_item = f.dtype.itemsize  # item size in bytes
+                write_items = len(f)  # number of items to write
+                nitems = self.libc.fwrite(f.tostring(), sizeof_item, write_items, self.data_file)
+                if nitems != write_items:
+                    raise ModuleError(self._object_name, "Write to file %s failed" % self.file_name)
+                # write marker
+                # self._write_marker(self.data.markers, self.data.block_time, self.data.sample_channel[0,0])
+                self.data.markers = self._write_marker(self.data.markers, self.data.block_time,
+                                                       self.data.sample_channel[0, 0], sctBreakDiff)
+
+                # update file sample counter
+                self.samples_written += samples
+
+                writetime = time.process_time() - t
+                # print("Write file: %.0f ms / %d Bytes / QSize %d"%(writetime*1000.0, nitems, self._input_queue.qsize()))
+            except Exception as e:
+                self.write_error = True  # indicate write error
+                self._thLock.release()  # release the thread lock because it is acquired by _close_recording()
+                self._close_recording()  # stop recording
+                self._thLock.acquire()
+                # ToDo: notify application
+                # self.send_event(ModuleEvent(self._object_name, EventType.ERROR,
+                #                             str(e),
+                #                             severity=ErrorSeverity.NOTIFY))
+
+        # update the global sample counter missing value
+        self.total_missing += missing_samples
+
+    def _write_marker(self, markers, blockdate, blocksamplecounter, sctBreakDiff):
+        """
+        Write marker to file
+        @param markers: list of marker objects (EEG_Marker)
+        @param blockdate: datetime object with start time of the current data block
+        @param blocksamplecounter: first sample counter value of the current data block
+        @param sctBreakDiff: 2-dimensional numpy array with sample counter values at index 0
+                             and number of missing samples at this counter at index 1
+        """
+        # insert "New Segment" marker as first marker and reset internal sample counters
+        if self.marker_counter == 0:
+            markers.insert(0, EEG_Marker(type="New Segment", date=True, position=blocksamplecounter))
+            self.start_sample = blocksamplecounter
+            self.total_missing = 0
+            self.samples_written = 0
+            self.start_time = blockdate
+
+        # adjust marker positions and insert new segment markers if necessary
+        new_segments = sctBreakDiff[:, :]
+        ns_cumulatedMissing = 0
+        output_markers = []
+
+        for marker in markers:
+            # are there a new segments before current marker position?
+            if self.marker_newseg and new_segments.shape[1]:
+                ns_position = new_segments[0, np.nonzero(new_segments[0] <= marker.position)[0]]
+                ns_missing = new_segments[1, np.nonzero(new_segments[0] <= marker.position)[0]]
+                # insert new segment markers
+                for ns in range(ns_position.size):
+                    ns_cumulatedMissing += ns_missing[ns]
+                    mkr = EEG_Marker(type="New Segment", date=True, position=ns_position[ns])
+                    output_markers.append(copy.deepcopy(mkr))
+                    # adjust the new segment marker time
+                    sampletime = (ns_position[ns] - self.start_sample) / self.params.sample_rate
+                    mkr.dt = self.start_time + datetime.timedelta(seconds=sampletime)
+                    # adjust position to file sample counter
+                    mkr.position = ns_position[ns] - self.start_sample - self.total_missing - ns_cumulatedMissing + 1
+                    # write new segment marker to file
+                    self._writeMarkerToFile(mkr, blockdate)
+                # remove handled new segments
+                new_segments = new_segments[:, np.nonzero(new_segments[0] > marker.position)[0]]
+
+            output_markers.append(copy.deepcopy(marker))
+            # missing samples up to marker position
+            miss = np.sum(sctBreakDiff[1, np.nonzero(sctBreakDiff[0] <= marker.position)[0]])
+            # adjust position to file sample counter
+            marker.position = marker.position - self.start_sample - self.total_missing - miss + 1
+            # write marker to file
+            self._writeMarkerToFile(marker, blockdate)
+
+        # append disregarded new segment markers
+        if self.marker_newseg and new_segments.shape[1]:
+            ns_position = new_segments[0, :]
+            ns_missing = new_segments[1, :]
+            # insert new segment markers
+            for ns in range(ns_position.size):
+                ns_cumulatedMissing += ns_missing[ns]
+                mkr = EEG_Marker(type="New Segment", date=True, position=ns_position[ns])
+                output_markers.append(copy.deepcopy(mkr))
+                # adjust the new segment marker time
+                sampletime = (ns_position[ns] - self.start_sample) / self.params.sample_rate
+                mkr.dt = self.start_time + datetime.timedelta(seconds=sampletime)
+                # adjust position to file sample counter
+                mkr.position = ns_position[ns] - self.start_sample - self.total_missing - ns_cumulatedMissing + 1
+                # write new segment marker to file
+                self._writeMarkerToFile(mkr, blockdate)
+
+        return output_markers
+
+    def _writeMarkerToFile(self, marker, blockdate):
+        """
+        Write single marker object to marker file
+        @param marker: EEG_Marker object
+        @param blockdate: datetime object with start time of the current data block
+        """
+        # consecutive marker number
+        self.marker_counter += 1
+        # Mkn=type,description,position,points,channel
+        m = u"Mk%d=%s,%s,%d,%d,%d" % (self.marker_counter,
+                                      marker.type,
+                                      marker.description,
+                                      marker.position,
+                                      marker.points,
+                                      marker.channel)
+        if marker.date:
+            try:
+                m += marker.dt.strftime(",%Y%m%d%H%M%S%f")
+            except:
+                m += blockdate.strftime(",%Y%m%d%H%M%S%f")
+        m += u"\n"
+        self.marker_file.write(m.encode('utf-8'))
+        self.marker_file.flush()
+
+    def _close_recording(self):
+        """
+        Close all EEG files
+        """
+        self._thLock.acquire()
+        if self.data_file != 0:
+            try:
+                self.libc.fclose(self.data_file)
+                self.marker_file.close()
+            except Exception as e:
+                print(f"Failed to close recording files: {e}")
+            self.data_file = 0
+            self.data_file = 0
+            # ToDo: self.online_cfg.set_recording_state(False)
+        self._thLock.release()
+
+
+    def process_output(self):
+        if not self.dataavailable:
+            return None
+        self.dataavailable = False
+        return self.data
+
+    def process_stop(self):
+        pass
+
+    def process_update(self, params):
+        pass
 
 
 """
 Storage module online GUI.
 """
+
 
 class _OnlineCfgPane:
     pass
@@ -126,6 +347,6 @@ class _OnlineCfgPane:
 Storage module configuration GUI.
 """
 
+
 class _ConfigurationPane:
     pass
-
