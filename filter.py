d166bbd317aea490d2bb81fa876d404e9584b1fa
Andrey Morozov <and.morozov.v@gmail.com>
2023-11-22 10:09:27 +0300

    add main method for the class flt_eeg

--- filter.py
+++ filter.py
@@ -1,3 +1,4 @@
+from scipy import signal
 from modbase import *
 from res import frmFilterConfig
 
@@ -9,6 +10,7 @@ class FLT_Eeg(ModuleBase):
     """
     Low, high pass and notch filter
     """
+
     def __init__(self, *args, **kwargs):
         super().__init__(name="EEG Filter", **kwargs)
 
@@ -34,8 +36,8 @@ class FLT_Eeg(ModuleBase):
         self.setDefault()
 
     def setDefault(self):
-        ''' Set all module parameters to default values
-        '''
+        """ Set all module parameters to default values
+        """
         # global filter values (for EEG-channels)
         self.lpGlobal = 0.0
         self.hpGlobal = 0.0
@@ -54,6 +56,185 @@ class FLT_Eeg(ModuleBase):
         """
         return _ConfigurationPane(self)
 
+    def _design_filter(self, frequency, type, slice):
+        """ Create filter settings for channel groups with equal filter parameters
+        @param frequency: filter frequeny in Hz
+        @param type: filter type, "low", "high" or "bandstop"
+        @param slice: channel group indices
+        @return: filter parameters and state vector
+        """
+        if (frequency == 0.0) or (frequency > self.samplefreq / 2.0):
+            return None
+        if type == "bandstop":
+            cut1 = (frequency - 1.0) / self.samplefreq * 2.0
+            cut2 = (frequency + 1.0) / self.samplefreq * 2.0
+            b, a = signal.filter_design.iirfilter(2, [cut1, cut2], btype=type, ftype='butter')
+            # b,a = signal.filter_design.iirfilter(2, [cut1, cut2], rs=40.0, rp=0.5, btype=type, ftype='elliptic')
+        else:
+            cut = frequency / self.samplefreq * 2.0
+            b, a = signal.filter_design.butter(self.filterorder, cut, btype=type)
+        zi = signal.lfiltic(b, a, (0.0,))
+        czi = np.resize(zi, (slice.stop - slice.start, len(zi)))
+        return {'slice': slice, 'a': a, 'b': b, 'zi': czi, 'frequency': frequency}
+
+    def process_update(self, params):
+        """ Calculate filter parameters for updated channels
+        """
+        # update the local reference
+        if self.params is None:
+            self.params = params
+        else:
+            # merge filter settings
+            for ch in params.channel_properties:
+                if ch.group == ChannelGroup.EEG:
+                    ch.lowpass = self.lpGlobal
+                    ch.highpass = self.hpGlobal
+                    ch.notchfilter = self.notchGlobal
+                else:
+                    # get local channel property by input number
+                    filterChannel = None
+                    for filter in self.params.channel_properties:
+                        if filter.input == ch.input and filter.inputgroup == ch.inputgroup:
+                            filterChannel = filter
+                            break
+                    if filterChannel is not None:
+                        ch.lowpass = filterChannel.lowpass
+                        ch.highpass = filterChannel.highpass
+                        ch.notchfilter = filterChannel.notchfilter
+            self.params = params
+
+        # reset filter
+        self.samplefreq = params.sample_rate
+        self.lpFilter = []
+        self.hpFilter = []
+        self.notchFilter = []
+
+        # nothing to filter
+        if len(params.channel_properties) == 0:
+            return params
+
+        # create channel slices and filter for continuous notch filters
+        notch = params.channel_properties[0].notchfilter
+        slc = slice(0, 1, 1)
+        for property in params.channel_properties[1::]:
+            if property.notchfilter == notch:
+                slc = slice(slc.start, slc.stop + 1, 1)
+            else:
+                # design filter
+                if notch:
+                    freq = self.notchFrequency
+                else:
+                    freq = 0.0
+                filter = self._design_filter(freq, 'bandstop', slc)
+                if filter is not None:
+                    self.notchFilter.append(filter)
+                slc = slice(slc.stop, slc.stop + 1, 1)
+                notch = property.notchfilter
+        if notch:
+            freq = self.notchFrequency
+        else:
+            freq = 0.0
+        filter = self._design_filter(freq, 'bandstop', slc)
+        if filter is not None:
+            self.notchFilter.append(filter)
+
+        # create channel slices and filter for continuous unique lowpass filter frequencies
+        freq = params.channel_properties[0].lowpass
+        slc = slice(0, 1, 1)
+        for property in params.channel_properties[1::]:
+            if property.lowpass == freq:
+                slc = slice(slc.start, slc.stop + 1, 1)
+            else:
+                # design filter
+                filter = self._design_filter(freq, 'low', slc)
+                if filter is not None:
+                    self.lpFilter.append(filter)
+                slc = slice(slc.stop, slc.stop + 1, 1)
+                freq = property.lowpass
+        filter = self._design_filter(freq, 'low', slc)
+        if filter is not None:
+            self.lpFilter.append(filter)
+
+        # create channel slices and filter for continuous unique highpass filter frequencies
+        freq = params.channel_properties[0].highpass
+        slc = slice(0, 1, 1)
+        for property in params.channel_properties[1::]:
+            if property.highpass == freq:
+                slc = slice(slc.start, slc.stop + 1, 1)
+            else:
+                # design filter
+                filter = self._design_filter(freq, 'high', slc)
+                if filter is not None:
+                    self.hpFilter.append(filter)
+                slc = slice(slc.stop, slc.stop + 1, 1)
+                freq = property.highpass
+        filter = self._design_filter(freq, 'high', slc)
+        if filter is not None:
+            self.hpFilter.append(filter)
+
+        # propagate down
+        return params
+
+    def process_input(self, datablock):
+        """
+        Filter all channel groups
+        """
+        self.dataavailable = True
+        self.data = datablock
+
+        # don't filter impedance values
+        if self.data.recording_mode == RecordingMode.IMPEDANCE:
+            return
+
+        # replace channel filter configuration within the data block with our modified configuration
+        for channel in range(len(self.data.channel_properties)):
+            self.data.channel_properties[channel].lowpass = self.params.channel_properties[channel].lowpass
+            self.data.channel_properties[channel].highpass = self.params.channel_properties[channel].highpass
+            self.data.channel_properties[channel].notchfilter = self.params.channel_properties[channel].notchfilter
+
+        # highpass filter
+        for flt in self.hpFilter:
+            self.data.eeg_channels[flt['slice']], flt['zi'] = \
+                signal.lfilter(flt['b'], flt['a'],
+                               self.data.eeg_channels[flt['slice']], zi=flt['zi'])
+
+        # lowpass filter
+        for flt in self.lpFilter:
+            self.data.eeg_channels[flt['slice']], flt['zi'] = \
+                signal.lfilter(flt['b'], flt['a'],
+                               self.data.eeg_channels[flt['slice']], zi=flt['zi'])
+
+        # notch filter
+        for flt in self.notchFilter:
+            self.data.eeg_channels[flt['slice']], flt['zi'] = \
+                signal.lfilter(flt['b'], flt['a'],
+                               self.data.eeg_channels[flt['slice']], zi=flt['zi'])
+
+    def process_output(self):
+        if not self.dataavailable:
+            return None
+        self.dataavailable = False
+        return self.data
+
+    def getXML(self):
+        """ Get module properties for XML configuration file
+        @return: objectify XML element::
+            e.g.
+            <EegFilter instance="0" version="1">
+                <notch_frequency>50.0</path>
+                ...
+            </EegFilter>
+        """
+        pass
+
+    def setXML(self, xml):
+        """
+        Set module properties from XML configuration file
+        @param xml: complete objectify XML configuration tree,
+        module will search for matching values
+        """
+        pass
+
 """
 Filter module configuration pane.
 """
@@ -63,6 +244,7 @@ class _ConfigurationPane(QFrame, frmFilterConfig.Ui_frmFilterConfig):
     """
     Module configuration pane
     """
+
     def __init__(self, filter, *args):
         super().__init__(filter, *args)
 
@@ -121,9 +303,11 @@ class _ConfigTableModel(QAbstractTableModel):
         self.lowpasslist = ['off', '10', '20', '30', '50', '100', '200', '500', '1000', '2000']
         self.highpasslist = ['off', '0.01', '0.02', '0.05', '0.1', '0.2', '0.5', '1', '2', '5', '10']
 
+
 class _ConfigItemDelegate(QStyledItemDelegate):
     """
     Combobox item editor.
     """
+
     def __init__(self, parent=None):
         super(_ConfigItemDelegate, self).__init__(parent)
